a4eb2f1c83cd76e0a3514c905085521dc575689a
Eric Gauderman <egauderman@gmail.com>
2014-02-01 01:46:39 -0800

    Added App Bar

--- usc_map/usc_map/MainPage.xaml.cs
+++ usc_map/usc_map/MainPage.xaml.cs
@@ -6,18 +6,41 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
+using Microsoft.Phone.Shell; // for App Bar
 using usc_map.Resources;
 
 namespace usc_map
 {
 	public partial class MainPage : PhoneApplicationPage
 	{
+		// App Bar stuff:
+		ApplicationBarIconButton _eventsToggle;
+		ApplicationBarIconButton _foodToggle;
+		ApplicationBarIconButton _studySpaceToggle;
+
 		// Constructor
 		public MainPage()
 		{
 			InitializeComponent();
 
+			ApplicationBar = new ApplicationBar();
+			ApplicationBar.Mode = ApplicationBarMode.Default;
+			ApplicationBar.IsVisible = true;
+
+			_eventsToggle = new ApplicationBarIconButton();
+			_eventsToggle.IconUri = new Uri("/Assets/feature.search.png", UriKind.Relative);
+			_eventsToggle.Text = "events";
+			ApplicationBar.Buttons.Add(_eventsToggle);
+
+			_foodToggle = new ApplicationBarIconButton();
+			_foodToggle.IconUri = new Uri("/Assets/feature.search.png", UriKind.Relative);
+			_foodToggle.Text = "food";
+			ApplicationBar.Buttons.Add(_foodToggle);
+
+			_studySpaceToggle = new ApplicationBarIconButton();
+			_studySpaceToggle.IconUri = new Uri("/Assets/feature.search.png", UriKind.Relative);
+			_studySpaceToggle.Text = "study spaces";
+			ApplicationBar.Buttons.Add(_studySpaceToggle);
 		}
 	}
 }
