6ac20a19f30b2800e6a1e7dbbc051d93199df76c
Eric Gauderman <egauderman@gmail.com>
2014-01-31 21:22:14 -0800

    Added blank WP8 template

---
+++ usc_map/usc_map/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using usc_map.Resources;
+
+namespace usc_map
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
---
+++ usc_map/usc_map/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using usc_map.Resources;
+
+namespace usc_map
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		// Constructor
+		public MainPage()
+		{
+			InitializeComponent();
+
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			textbox0.Text = "you clicked me";
+		}
+	}
+}
